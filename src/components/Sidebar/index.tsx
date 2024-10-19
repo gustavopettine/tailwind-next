2e61a604696d5025663eba934a32683963cec0b6
gupettine <gupettine@gmail.com>
2024-10-19 20:08:25 -0300

    feature: create user profile

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -2,6 +2,7 @@ import { Search, BarChart, CheckSquare, Flag, Home, SquareStack, Users, LifeBuoy
 import { Logo } from "./Logo";
 import { NavItem } from "./NavItem";
 import { UsedSpaceWidget } from "./UsedSpaceWidget";
+import { Profile } from "./Profile";
 
 export function Sidebar() {
   return <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
@@ -32,6 +33,10 @@ export function Sidebar() {
       </nav>
 
       <UsedSpaceWidget />
+
+      <div className="h-px bg-zinc-200" />
+      
+      <Profile />
     </div>
   </aside>
 }
